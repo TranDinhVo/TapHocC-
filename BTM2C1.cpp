7995c26614ba5aef4bc6eb9374a96eeca5c82f85
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-02 14:06:33 +0700

    Update BTM2C1.cpp

--- BTM2C1.cpp
+++ BTM2C1.cpp
@@ -1,3 +1,5 @@
+bai 1
+
 #include <iostream>
 using namespace std;
 
@@ -27,4 +29,40 @@ int main()
   cout << " Tong S = " << s;
       
     return 0;
+} 
+ 
+Bai 2
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int arr[10], n;
+   cout << "n = ";
+   cin >> n;
+
+  cout << "Nhap mang ";
+  for (int i = 0 ; i < n; i++)
+    {
+      cin >> arr[i];
+    }
+   int Max = arr[0];
+  for (int i = 1 ; i < n; i++)
+    { 
+      if ( Max < arr[i])
+      {
+        Max= arr[i];
+      }
+    }
+  cout << "Gia tri lon nhat la: " << Max;
+    return 0;
 }
+
+
+
+Bai 3 
