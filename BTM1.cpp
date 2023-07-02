afc58a4885d30f3a6cbc160e474a754983ca90ef
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-02 14:08:43 +0700

    Update BTM1.cpp

--- BTM1.cpp
+++ BTM1.cpp
@@ -69,6 +69,47 @@ int main()
 
 
 BAI 3
+   #include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int numbers[20];
+   int n, x , y;
+   cout << "n = ";
+   cin >> n;  
+
+   cout << "Nhap mang ";
+  for ( int i = 0; i < n; i++)
+    {
+      cin >> numbers[i];
+    }
+   cout << "x = ";
+   cin >> x;
+  cout << "y = ";
+   cin >> y;  
+   for ( int i = n ; i > x ; i--)
+     {
+       numbers[i] = numbers[i - 1];
+     }
+   n++;
+  numbers[x] = y;
+ 
+
+  cout << "Mang sau khi them la: ";
+  for ( int i=0; i < n; i++)
+    {
+      cout << numbers[i] << " ";
+    }
+  return 0;
+  
+} 
+
+
+BAI 4
+
+   
    
 
 
