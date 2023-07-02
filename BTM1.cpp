050da183c91685fdc5badabfdff19361dec823d1
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-02 14:09:19 +0700

    Update BTM1.cpp

--- BTM1.cpp
+++ BTM1.cpp
@@ -108,6 +108,70 @@ int main()
 
 
 BAI 4
+   #include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10][10], n, m, s = 0;
+    cout << "n =  ";
+    cin >> n;
+    cout << "m =  ";
+    cin >> m;
+
+    cout << " Nhap Mang "<< endl;
+   for ( int i = 0; i < n; i++)
+     {
+       for ( int j = 0; j < m; j++)
+         {
+           cin >> arr[i][j];
+         } 
+     }
+   for ( int i = 0; i < n; i++)
+     {
+       for ( int j = 0; j < m; j++)
+         {
+           s += arr[i][j];
+         } 
+     }
+  cout << " Tong S = " << s;
+      
+    return 0;
+} 
+ 
+BAI 5
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int arr[10], n;
+   cout << "n = ";
+   cin >> n;
+
+  cout << "Nhap mang ";
+  for (int i = 0 ; i < n; i++)
+    {
+      cin >> arr[i];
+    }
+   int Max = arr[0];
+  for (int i = 1 ; i < n; i++)
+    { 
+      if ( Max < arr[i])
+      {
+        Max= arr[i];
+      }
+    }
+  cout << "Gia tri lon nhat la: " << Max;
+    return 0;
+}
+
+
 
    
    
