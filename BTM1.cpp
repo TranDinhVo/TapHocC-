94da57636c5a4b380d1b7b3a84e9ed05bd6d78ba
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-02 15:21:11 +0700

    Update BTM1.cpp

--- BTM1.cpp
+++ BTM1.cpp
@@ -247,6 +247,42 @@ cout << s;
 }    
     
    BAI TAP CHUONG 4
+      BAI 1 
+      #include <iostream>
+using namespace std;
+
+int main()
+{
+   int arr[10], n;
+   cout << "n = ";
+   cin >> n;
+   cout << " Nhap mang: ";
+  for (int i = 0 ; i < n ; i++)
+    {
+      cin >> arr[i];
+    }
+  for (int i = n - 1 ; i > 0; i--)
+    {
+      for (int j = 0 ; j < i ; j++)
+        {
+          if (arr[j] < arr[j+1])
+            {
+              int a=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=a;
+            }
+        }
+       
+    }
+  cout << " Mang sau khi sap xep la: "
+  for (int i = 0 ; i < n ; i++)
+    {
+       cout << arr[i] << " ";
+    }
+  return 0;
+}
+
+    BAI 2
       
    
 
