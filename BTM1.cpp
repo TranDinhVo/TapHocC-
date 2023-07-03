5e6a3dae6239d7116b11afa953b814d248a16c36
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-03 07:52:18 +0700

    Update BTM1+2.cpp

--- BTM1.cpp
+++ BTM1.cpp
@@ -283,6 +283,122 @@ int main()
 }
 
     BAI 2
+       #include <iostream>
+using namespace std;
+
+int main()
+{
+   int arr[10], n, k;
+   cout << "n = ";
+   cin >> n;
+   cout << " Nhap mang: ";
+  for (int i = 0 ; i < n ; i++)
+    {
+      cin >> arr[i];
+    }
+   cout << "k = ";
+   cin >> k;
+    int x = - 1 ,y = - 1;
+
+    for (int i = 0 ; i < n ; i++)
+      {  for (int j = 0; j < n; j++)
+          { 
+            if (i != j && arr[i] + arr[j] == k)
+            { 
+              x = i;
+              y = j;
+              break;
+            }
+          }
+        if (x != -1 && y != -1)
+          break;
+        
+      }
+  if ( x !=-1 && y !=-1)
+  {
+    cout << "Ket qua " << x << " " << y;
+  }  else {
+    cout << "khong co";
+  }
+  return 0;
+}
+
+     BAI3 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, m, matran1[10][10], matran2[10][10];
+  cout << "n = ";
+  cin >> n;
+  cout << "m = ";
+  cin >> m;
+
+  cout << "Nhap ma tran 1:\n";
+    for(int i=0;i<n;i++)
+      {
+        for(int j=0;j<m;j++)
+          {
+            cin >> matran1[i][j];
+          }
+      }
+  cout << "Nhap ma tran 2:\n";
+  for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<m;j++)
+        {
+          cin >> matran2[i][j];
+        }
+    }
+   cout << "Tong hai ma tran la:\n";
+ for(int i=0;i<n;i++)
+   {
+     for(int j=0;j<m;j++)
+       {
+         cout << matran1[i][j] + matran2[i][j] << " ";
+       }
+     cout << endl;
+   }
+ 
+  return 0;
+}
+
+   BAI4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int s =0, n, m, arr[10][10];
+  cout << "n = ";
+  cin >> n;
+  cout << "m = ";
+  cin >> m;
+
+  cout << "Nhap ma tran :\n";
+    for(int i=0;i<n;i++)
+      {
+        for(int j=0;j<m;j++)
+          {
+            cin >> arr[i][j];
+          }
+      }
+  for (int i=0;i<n;i++)
+    {
+      for (int j=0;j<m;j++)
+        {
+          if (arr[i][j]%2==1)
+          {
+            s += arr[i][j];
+          }
+        }
+        
+    }
+  cout << "Tong so do le la: " << s;
+  return 0;
+}
+
       
    
 
