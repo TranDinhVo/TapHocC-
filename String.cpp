591dc67cbbb1c448d24606ee813088fb10edad32
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-03 09:19:26 +0700

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -93,12 +93,13 @@ int main()
      {
        str.erase(str.length() - 1.1);
      }
-  for (int i = 0; i < str.length(); i++ )
+  int i = 0;
+    while (i < str.length())
     {
-      if ( str[i] == ' ' && str[i + 1] == ' ')
-      {
-        str.erase(i, 1);
-      }  else {  i++; }
+        if ( str[i] == ' ' && str[i + 1] == ' ')
+        {
+            str.erase(i,1);
+        }  else     {   i++;  } 
     }
  if (str[0] != ' ')
  {
