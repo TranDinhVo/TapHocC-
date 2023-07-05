e44ea75809dba60cd9571d462f13893821823c2e
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-07-05 22:48:15 +0700

    Update BTC7.cpp

--- BTC7.cpp
+++ BTC7.cpp
@@ -155,6 +155,84 @@ void remove ( int* &arr, int &size, int delete_index)
 
 
 
+   BAI3  
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+void input_integer(string label, int &n);
+void input_array ( int *arr, int size);
+void output_array ( int *arr, int size);
+void update( int* &arr, int &size, int update_index, int new_element);
+
+
+int main()
+{ 
+  int n, y, x;
+  input_integer( "n = ", n);
+  int *numbers = new int[n];
+  cout << " Nhap Mang: ";
+  input_array(numbers, n);
+  input_integer( "y = ", y);
+  input_integer( "x = ", x);
+  update(numbers, n, x ,y);
+  cout << "Mang sau khi them la: ";
+  output_array(numbers, n);
+  
+
+  
+  return 0;
+}
+void input_integer(string label, int &n)
+{
+  cout << label;
+  cin >> n;
+}
+void input_array ( int *arr, int size)
+{
+  for ( int i = 0; i < size; i++)
+  {
+    cin >> arr[i];
+  }
+}
+void output_array ( int *arr, int size)
+{
+  for ( int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
+void update( int* &arr, int &size, int update_index, int new_element)
+{  
+   // Tao mang moi
+  
+  int new_size = size + 1;
+  int *new_arr = new int[size + 1];
+   // Copi mang cu sang mang moi
+  for ( int i = 0; i < size ; i++)
+    {
+      new_arr[i] = arr[i];
+    }
+  // them phan tu moi vao mang
+  for ( int i = new_size - 1 ; i > update_index ; i--)
+    {
+      new_arr[i] = new_arr[i-1];
+    }
+  new_arr[update_index] = new_element;
+  // gan mang moi cho con tro cu va tang size
+  delete[] arr;
+  arr = new_arr;
+  size = new_size;
+  
+}
+
+
+
+
+
+
+
 
 
 
